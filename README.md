22314758a6221a6b4ab60b4b2d0884b04ceb5a25
Behrouz <behrouz.safari@gmail.com>
2024-02-17 12:31:22 +0100

    fix eot

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 **Author:** [Behrouz Safari](https://astrodatascience.net/)<br/>
 
 # Live astronomical clock
+
+[Link1](https://astrodatascience.net/live-clock/) | [Link2](https://liveclock.pythonanywhere.com/)
--- live_clock.py
+++ live_clock.py
@@ -72,7 +72,7 @@ class Clock:
         self.mean_solar_time, self.true_solar_time = \
                 solar_time(t=self.t, lon=self.lon, eot_df=eot_df)
         self.noon = get_noon(t=self.t, lon=self.lon, eot_df=eot_df)
-        self.eot = (self.mean_solar_time - self.true_solar_time).total_seconds()/60
+        self.eot = (self.true_solar_time - self.mean_solar_time).total_seconds()/60
         self.eot_str = self.__format_eot()
         self.lst_deg = get_lst(self.t, self.lon)
         self.lst = self.__format_lst()
